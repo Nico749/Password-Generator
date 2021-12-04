2cd0cec31e1addbb5a15ee976074eab513459bde
Nico Pasqualini <pasqualinin9@gmail.com>
2021-12-04 22:56:05 +1100

    setting prompt and alert

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function generateMsg(){
+  
+
+var userInput = prompt(`Which criteria do you want to inlcude in the password? 
+1. A mixture of both uppercase and lowercase letters.
+2. A mixture of letters and numbers.
+3. Inclusion of at least one special character, e.g., ! @ # ? ] `);
+
+if (userInput==1||userInput==2||userInput==3){
+  var userChoice=prompt(`Password length (8-->128 characters):`)
+   if (userChoice>=8 && userChoice<=128){prompt('what do you want')}
+   else{alert(`Choice not valid`)}
+    
+}
+else{
+  alert(`Choice not valid`)
+}
+} 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
