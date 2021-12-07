1ad5b7962b459779810d91990e9845156ae05da0
Nico Pasqualini <pasqualinin9@gmail.com>
2021-12-07 11:42:06 +1100

    Updating js functions

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 function generateMsg(){
   
+alert(`Password should include:
+- A mixture of both uppercase and lowercase letters.
+- A mixture of letters and numbers.
+- Inclusion of at least one special character, e.g., ! @ # ? ] `);
 
-var userInput = prompt(`Which criteria do you want to inlcude in the password? 
-1. A mixture of both uppercase and lowercase letters.
-2. A mixture of letters and numbers.
-3. Inclusion of at least one special character, e.g., ! @ # ? ] `);
-
-if (userInput==1||userInput==2||userInput==3){
-  var userChoice=prompt(`Password length (8-->128 characters):`)
-   if (userChoice>=8 && userChoice<=128){prompt('what do you want')}
-   else{alert(`Choice not valid`)}
+var userChoice=prompt(`Password length (8-->128 characters):`)
+   
+if (userChoice>=8 && userChoice<=128){
+     var userDecision= prompt(`Would you like to include lowercase, uppercase, symbols and numbers in the password? Y/N (If not it would be impossible to create a password)`)
+     if(userDecision=="Y"){
+     //writePassword();
+     //var password=generatePwd()
+     //alert(password)
+     }
     
-}
-else{
-  alert(`Choice not valid`)
-}
-} 
+     else{alert(`Choice not valid`)}}
+   
+else{alert(`Choice not valid`)}
+    
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePwd();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Password generation function
+function generatePwd() {
+  var length = userChoice;
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#?*]|&";
+      defaultValue = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      defaultValue += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return defaultValue;
+}}
