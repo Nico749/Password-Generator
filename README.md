9ebe8b9e876668836716bdafd107690d9e1d53e7
Nico Pasqualini <pasqualinin9@gmail.com>
2021-12-08 12:27:06 +1100

    Updating readme file and minor changes input validation

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Password-Generator
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Demo](#demo)
+* [Project Status](#project-status)
+* [Room for Improvement](#room-for-improvement)
+* [Contact Me](#contact-me)
+
+## General info
+
+The goal of the project is to create a program that allows employees to create a safe and strong password whenever they need to protect their data. With a series of prompt, they are asked to accept some criteria like the number of characters, the use of symbolsa and numbers and then after they have been validated, a password is created and displayed 
+
+## Technologies
+
+Project is created with:
+* HTML
+* JavaScript
+* CSS
+
+## Demo
+![Working demo](./Screenshot-demo.png)
+
+## Project Status
+* Complete
+
+## Room for Improvement
+Room for improvement:
+* Optimize input validation 
+
+## Contact me 
+Created by [Nico Pasqualini](https://nico749.github.io/Personal-portfolio-/) - feel free to contact me!
+
--- script.js
+++ script.js
@@ -6,26 +6,26 @@ alert(`A strong password should include:
 - Inclusion of at least one special character, e.g., ! @ # ? ] `);
 
 var userChoice=prompt(`Password length (8-->128 characters):`)
-   
+//input validation    
 if (userChoice>=8 && userChoice<=128){
      
-     var userDecision= prompt(`Would you like to include lowercase, uppercase, symbols and numbers in the password? Y/N (If not it would be impossible to create a password)`)
-     if(userDecision=="Y"){
+     var userDec= prompt(`Would you like to include lowercase, uppercase, symbols and numbers in the password? Y/N (If not it would be impossible to create a password)`)
+     var userDecision =userDec.replace(/^\s+|\s+$/gm,'');//remove all white spaces before the user input
+     if(userDecision=="Y"||userDecision=="yes"||userDecision=="y"||userDecision=="Yes"){
      writePassword();
      }
      else{alert(`Choice not valid, impossible to create a password`)}}
    
-else{alert(`Password should include at least 8 characters and maximum 128`)}
-    
+else if (isNaN(userChoice)){alert(`You need to insert a numeric value`)}
+
+else {alert(`Password should include at least 8 characters and maximum 128`)}   
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePwd();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  passwordText.value = "Your secure password is: "+password;
 }
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
 
 // Password generation function
 function generatePwd() {
